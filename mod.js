bd432d0037f62f1a666b9b01e1b8858f014f6a82
Masaabu <Masa2525ym@gmail.com>
2023-12-15 13:27:53 +0900

    update

--- mod.js
+++ mod.js
@@ -1,19 +1,28 @@
-const sbApi = [
+const sbModList = [
+    /*{
+      title: 'SB MOD v1.0',
+      description: 'by Masaabu-YT',
+      id: 940232456,
+      tag: '"textFrame orange">公式<'
+    }*/
+]
+
+const sbApiList = [
     {
       title: 'SBAPI',
       description: 'ビル経営ゲームの拡張性を上げるツール<br>by dmmo-com-jp',
-      id: '933515639',
+      id: 933515639,
       tag: '"textFrame blue">前提MOD<'
     },
     {
       title: 'cmdplus',
       description: 'SBAPIのpostに依存した、コマンド強化modです。<br>by dmmo-com-jp',
-      id: '939787547'
+      id: 939787547
     },
     {
       title: '隕石経営ゲーム',
       description: '隕石！！<br>by banana_877_',
-      id: '933713134'
+      id: 933713134
     }
 ];
 
@@ -21,28 +30,28 @@ const sbAddonsList = [
     {
       title: 'SB Addons',
       description: 'by -Masaabu-,Masaabu-YT',
-      id: '790969288',
+      id: 790969288,
       tag: '"textFrame orange">公式<'
     },
     {
       title: 'SB Addons+',
       description: 'by dmmo-com-jp',
-      id: '791408146'
+      id: 791408146
     },
     {
       title: '隕石アドオン',
       description: 'by dmmo-com-jp',
-      id: '792121597'
+      id: 792121597
     },
     {
       title: 'Re:BN addon',
       description: 'by banana_877_',
-      id: '817892400'
+      id: 817892400
     },
     {
       title: 'SB Addons Remix',
       description: 'by yoshikunTA',
-      id: '795165690'
+      id: 795165690
     }
 ];
 
@@ -50,17 +59,17 @@ const sbAddonsApiList = [
     {
       title: 'SBAPI',
       description: 'お金の管理や幸福度の管理を簡単に行えます。<br>by dmmo-com-jp',
-      id: '926411096',
+      id: 926411096,
       tag: '"textFrame blue">前提MOD<'
     },
     {
       title: '天候復活アドオン',
       description: '天候を6.0に復活させます。<br>by dmmo-com-jp',
-      id: '933340996'
+      id: 933340996
     }
 ];
 
-const section = [["sbApiList",sbApi],["sbAddonsList",sbAddonsList],["sbAddonsApiList",sbAddonsApiList]]
+const section = [["sbModList",sbModList],["sbApiList",sbApiList],["sbAddonsList",sbAddonsList],["sbAddonsApiList",sbAddonsApiList]]
 
 function generateList() {
     section.forEach(element => {
