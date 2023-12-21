1957996b2bae22bc5ccc6428d8fbb107a77c2ffa
Fun117 <141471450+Fun117@users.noreply.github.com>
2023-12-21 11:59:48 +0900

    upload

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,19 +1,10 @@
-var obj = convertCookieToObject(document.cookie);//クッキーを読み出す
-if(obj.html_class !== 'dark' || obj.html_class !== 'light'){
-    document.cookie = 'html_class=dark; path=/';
-    //console.log('error')
-}
-document.cookie = 'html_class=dark; path=/';
-document.getElementById('html').className = ('dark')
-var obj = convertCookieToObject(document.cookie);//クッキーを読み出す
+const obj = convertCookieToObject(document.cookie);//クッキーを読み出す
 let html_class = obj.html_class;//クッキーの中の""html_class"を取得
 document.getElementById('html').className = (html_class)//htmlのclassを変更
 
-
-
 function class_change(){
     if(convertCookieToObject(document.cookie).html_class === 'dark'){//クッキーの"html_class"が"dark"なら
-        //document.getElementById('html').className = ('light')//"html"のclassを"light"
+        document.getElementById('html').className = ('light')//"html"のclassを"light"
     }else{//ではない
         document.getElementById('html').className = ('dark')//"html"のclassを"dark"
     }
@@ -28,7 +19,6 @@ function class_modeCiikiesSeve(){//クラスモード変更検知
         const obj = convertCookieToObject(document.cookie);//クッキーを読み出す
     }
 }
-
 
 function convertCookieToObject(cookies){//クッキーの解析
     const cookieItem = cookies.split(';');
