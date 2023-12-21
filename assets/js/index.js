56b477c5972645383dbeb9edde368276f0ea129a
Fun117 <141471450+Fun117@users.noreply.github.com>
2023-12-21 16:47:09 +0900

    upload

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+const targetDate = new Date('2023-12-01T17:00:00+09:00').getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById('countdown').innerHTML = `公開まで： ${days}日 ${hours}:${minutes}:${seconds}`;
+
+  if (distance < 0) {
+    document.getElementById('countdown').innerHTML = 'Scratchで公開中！';
+  } else {
+    setTimeout(updateCountdown, 1000);
+  }
+}
+
+updateCountdown();
