7df1427a67c8db45a23c4a12f687dbd22df24da6
Fun117 <141471450+Fun117@users.noreply.github.com>
2023-12-22 23:57:47 +0900

    Implementation of new functions
    Added button to read more comments.
    MOD information is now completely managed by object variables.
    Changed the mechanism for processing MOD information.

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,17 +1,24 @@
 const apiURL = 'https://script.google.com/macros/s/AKfycbxm-7lnqXUimeXMYN45Ebj5iY5tyKJW0pL2tWoGi84MKGCiBXr0gXspvuyUQo-JBFraCQ/exec';
 
+var commentCount = 0;
+var commentShowC = 5;
+
 async function loadData() {
   const commentsContainer = document.getElementById('commentsContainer');
   const response = await fetch(apiURL);
   const data = await response.json();
 
   data.reverse().forEach(entry => {
+    const commentLi = document.createElement('li');
+    commentLi.id = (`comment_${commentCount}`)
+    commentLi.classList.add('list-item');
+
     const commentDiv = document.createElement('div');
     commentDiv.classList.add('comment');
 
     const usernamePara = document.createElement('p');
     usernamePara.classList.add('username');
-    usernamePara.textContent = entry.name;
+    usernamePara.textContent = (entry.name+' #'+commentCount);
 
       const checkDiv = document.createElement('p');
     if (entry.check) {
@@ -30,9 +37,54 @@ async function loadData() {
     commentDiv.appendChild(usernamePara)
     commentDiv.appendChild(timestampPara);
     commentDiv.appendChild(commentPara);
+    commentLi.appendChild(commentDiv);
     
-    commentsContainer.appendChild(commentDiv);
+    commentsContainer.appendChild(commentLi);
+    commentCount = commentCount+1;
   });
+  if(commentCount > 5){
+    loadAddComment('set','5');
+  }else{
+    loadAddComment('set',commentCount);
+  }
 }
 
 loadData();
+
+
+
+
+
+// コメントをさらに読み込むボタン
+
+function loadAddComment(mode,value){
+  value=value-0;
+  if(mode === 'set'){
+    commentShowC = value - 0;
+    for (let i = commentShowC; i < commentCount; i++) {
+      document.getElementById(`comment_${i}`).style.display=('none');
+    };
+  }else{if(mode === 'add'){
+    commentShowC = commentShowC + value;
+    if(commentShowC > commentCount){
+      commentShowC = commentCount;
+    };
+    console.log(commentShowC)
+    for (let i = 0; i < commentShowC -1; i++) {
+      document.getElementById(`comment_${i}`).style.display=('block');
+    };
+    console.log('displayComment = '+commentShowC+'/'+commentCount);
+  }else{
+    console.error('The requested mode does not exist.');
+  };
+  };
+};
+
+setInterval(() => {
+  document.getElementById('commentShowCHtml').innerHTML=(`表示中のコメント：${commentShowC}/${commentCount}`)
+  if(commentShowC === commentCount){
+    document.getElementById('loadAddComment').style.display=('none');
+  }else{
+    document.getElementById('loadAddComment').style.display=('block');
+  }
+}, 500);
