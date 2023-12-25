52b9f9bf53da65e61534d1547d3b0eac0cb7fd88
Fun117 <141471450+Fun117@users.noreply.github.com>
2023-12-25 10:24:16 +0900

    updata

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -4,75 +4,178 @@ var commentCount = 0;
 var commentShowC = 0;
 var commentListLoadSetC = 20;//この変数に初めの表示するコメント数を設定
 commentListLoadSetC - 0;
+var repCommentsCount = 0;
+var repComments=(JSON.parse(`{}`));
+
+//console.log(repComments)
 
 async function loadData() {
   const commentsContainer = document.getElementById('commentsContainer');
   const response = await fetch(apiURL);
-  const data = await response.json();
-
+  var data = await response.json();
   data.reverse().forEach(entry => {
-    const commentLi = document.createElement('li');
-    commentLi.id = (`comment_${commentCount}`)
-    commentLi.classList.add('list-item');
 
-    const commentDiv = document.createElement('div');
-    commentDiv.classList.add('comment');
-
-    const usernamePara = document.createElement('p');
-    usernamePara.classList.add('username');
-    usernamePara.textContent = (entry.name);
-    //usernamePara.textContent = (entry.name+' #'+commentCount);//コメントの読み込み番号を表示
-
-    const checkDiv = document.createElement('p');
-    if (entry.check) {
-      checkDiv.style.color = '#f26b30';
-      checkDiv.textContent = "認証済み";
+    if(entry.rep) {
+      repComments[`${repCommentsCount}`]=({"timestamp":entry.timestamp,"name":entry.name,"comment":entry.comment,"rep":entry.rep,"uuid":entry.uuid,"check":entry.check});
+      repCommentsCount=repCommentsCount+1;
+    }else{
+      const commentLi = document.createElement('li');
+      commentLi.id = (`comment_${commentCount}`)
+      commentLi.classList.add('list-item');
+  
+      const commentDiv = document.createElement('div');
+      commentDiv.classList.add('comment');
+  
+      const commentRepDiv = document.createElement('div');
+      commentRepDiv.id = (`comment_${entry.uuid}`)
+  
+      const usernamePara = document.createElement('p');
+      usernamePara.classList.add('username');
+      usernamePara.textContent = (entry.name);
+      //usernamePara.textContent = (entry.name+' #'+commentCount);//コメントの読み込み番号を表示
+  
+      const checkDiv = document.createElement('p');
+      if (entry.check) {
+        checkDiv.style.color = '#f26b30';
+        checkDiv.textContent = "認証済み";
+      }
+  
+      const timestampPara = document.createElement('p');
+      timestampPara.classList.add('timestamp');
+      timestampPara.textContent = new Date(entry.timestamp).toLocaleString();
+  
+      const commentPara = document.createElement('p');
+      commentPara.textContent = entry.comment;
+  
+      const uuidDivSpe = document.createElement('p');
+      uuidDivSpe.classList.add('reportSpace')
+  
+      const uuidDiv = document.createElement('p');
+      uuidDiv.style.textAlign=('right')
+      uuidDiv.classList.add('report')
+  
+  
+      const repBtn = document.createElement('button');
+      repBtn.textContent = `返信する`;
+      repBtn.style.margin = `0 0 0 10px`;
+      repBtn.style.color = `var(--text-2)`;
+      repBtn.addEventListener('click', () => {
+        commentRep(entry.uuid);
+      });
+      uuidDiv.appendChild(repBtn);
+  
+      const uuidBtn = document.createElement('button');
+      uuidBtn.textContent = `通報する`;
+      uuidBtn.style.margin = `0 0 0 10px`;
+      uuidBtn.style.color = `var(--text-2)`;
+      uuidBtn.addEventListener('click', () => {
+        commentReport(entry.uuid);
+      });
+      uuidDiv.appendChild(uuidBtn);
+  
+      usernamePara.appendChild(checkDiv);
+      commentDiv.appendChild(usernamePara);
+      commentDiv.appendChild(timestampPara);
+      commentDiv.appendChild(commentPara);
+      commentDiv.appendChild(uuidDivSpe);
+      commentDiv.appendChild(uuidDiv);
+      commentLi.appendChild(commentDiv);
+      commentLi.appendChild(commentRepDiv);
+      
+      commentsContainer.appendChild(commentLi);
+      commentCount = commentCount+1;
     }
-
-    const timestampPara = document.createElement('p');
-    timestampPara.classList.add('timestamp');
-    timestampPara.textContent = new Date(entry.timestamp).toLocaleString();
-
-    const commentPara = document.createElement('p');
-    commentPara.textContent = entry.comment;
-
-    const uuidDivSpe = document.createElement('p');
-    uuidDivSpe.classList.add('reportSpace')
-
-    const uuidDiv = document.createElement('p');
-    uuidDiv.style.textAlign=('right')
-    uuidDiv.classList.add('report')
-
-    const uuidBtn = document.createElement('button');
-    uuidBtn.textContent = `通報する`;
-    uuidBtn.style.color = `var(--text-2)`;
-    uuidBtn.addEventListener('click', () => {
-      commentReport(entry.uuid);
-    });
-    uuidDiv.appendChild(uuidBtn);
-
-    usernamePara.appendChild(checkDiv);
-    commentDiv.appendChild(usernamePara);
-    commentDiv.appendChild(timestampPara);
-    commentDiv.appendChild(commentPara);
-    commentDiv.appendChild(uuidDivSpe);
-    commentDiv.appendChild(uuidDiv);
-    commentLi.appendChild(commentDiv);
-    
-    commentsContainer.appendChild(commentLi);
-    commentCount = commentCount+1;
   });
   if(commentCount >　commentListLoadSetC){
     loadAddComment('set',commentListLoadSetC);
   }else{
     loadAddComment('set',commentCount);
-  }
-}
+  };
+  //repCommentLoadData();
+};
+
+function repCommentLoadData(){
+  //repCommentsCount=repCommentsCount-1;
+  for(let i = 0; i < repCommentsCount; i++){
+    if(i===(repCommentsCount-1)){
+
+    }else{
+      let repHostComment = document.getElementById(`comment_${repComments[i].rep}`);
+      console.log(i+' / '+repCommentsCount);
+  
+      const commentLi = document.createElement('li');
+      commentLi.id = (`comment_0`);
+      commentLi.classList.add('list-item');
+  
+      const commentDiv = document.createElement('div');
+      commentDiv.classList.add('comment','commentRep');
+  
+      const commentRepDiv = document.createElement('div');
+      commentRepDiv.id = (`comment_${repComments[i].uuid}`);
+  
+      const usernamePara = document.createElement('p');
+      usernamePara.classList.add('username');
+      usernamePara.textContent = (repComments[i].name);
+      //usernamePara.textContent = (repComments[i].name+' #'+commentCount);//コメントの読み込み番号を表示
+  
+      const checkDiv = document.createElement('p');
+      if (repComments[i].check) {
+        checkDiv.style.color = '#f26b30';
+        checkDiv.textContent = "認証済み";
+      }
+  
+      const timestampPara = document.createElement('p');
+      timestampPara.classList.add('timestamp');
+      timestampPara.textContent = new Date(repComments[i].timestamp).toLocaleString();
+  
+      const commentPara = document.createElement('p');
+      commentPara.textContent = repComments[i].comment;
+  
+      const uuidDivSpe = document.createElement('p');
+      uuidDivSpe.classList.add('reportSpace')
+  
+      const uuidDiv = document.createElement('p');
+      uuidDiv.style.textAlign=('right')
+      uuidDiv.classList.add('report')
+  
+  
+      const repBtn = document.createElement('button');
+      repBtn.textContent = `返信する`;
+      repBtn.style.margin = `0 0 0 10px`;
+      repBtn.style.color = `var(--text-2)`;
+      //repBtn.addEventListener('click', () => {
+      //  commentRep(repComments[i].rep);
+      //});
+      uuidDiv.appendChild(repBtn);
+  
+      const uuidBtn = document.createElement('button');
+      uuidBtn.textContent = `通報する`;
+      uuidBtn.style.margin = `0 0 0 10px`;
+      uuidBtn.style.color = `var(--text-2)`;
+      uuidDiv.appendChild(uuidBtn);
+  
+      usernamePara.appendChild(checkDiv);
+      commentDiv.appendChild(usernamePara);
+      commentDiv.appendChild(timestampPara);
+      commentDiv.appendChild(commentPara);
+      commentDiv.appendChild(uuidDivSpe);
+      commentDiv.appendChild(uuidDiv);
+      commentLi.appendChild(commentDiv);
+      commentLi.appendChild(commentRepDiv);
+      
+      repHostComment.appendChild(commentLi);
+      commentCount = commentCount+1;
+    }
+  };
+  if(commentCount >　commentListLoadSetC){
+    loadAddComment('set',commentListLoadSetC);
+  }else{
+    loadAddComment('set',commentCount);
+  };
+};
+
 
 loadData();
-
-
-
 
 
 // コメントをさらに読み込むボタン
@@ -125,4 +228,9 @@ function commentReport(id){
   }else{
     alert('入力値が正しくありません！');
   }
+};
+
+function commentRep(id){
+  window.location='#コメント欄';
+  document.getElementById('field-repCommentId').value=`${id}`;
 };
---
+++ assets/js/content/news.js
@@ -0,0 +1,247 @@
+// Set the file path for this directory
+var windowUrl = window.location.pathname;
+var windowUrlSr = 0;
+if((window.location.protocol) === 'https:'){
+    windowUrlSr =  ( windowUrl.match( /[/]/g ) || [] ).length - 3;
+}else{
+    console.warn('Runs in editor mode because the protocol is not "https".')
+    windowUrlSr =  ( windowUrl.match( /[/]/g ) || [] ).length - 2;
+};
+var windowFileDirectlyPass = "";
+if(windowUrlSr > 0){
+    for (let i = 0; i < windowUrlSr; i++) {
+        windowFileDirectlyPass = windowFileDirectlyPass + `./.`;
+    }
+    windowFileDirectlyPass = `.`+windowFileDirectlyPass+`.`;
+}else{
+    windowFileDirectlyPass = `.`;
+};
+
+// js 処理
+var serverJsonData = "";
+var newsObj=(JSON.parse(`
+{
+
+}
+`));
+
+function jsonListLoad(page){
+    if(page===''){
+        page='true';
+    };
+
+    var tomorrow={};
+    var tomorrowD = new Date()
+    tomorrow.getFullYear=(tomorrowD.getFullYear());
+    tomorrow.getMonth=(tomorrowD.getMonth() + 1);
+    tomorrow.getDate=(tomorrowD.getDate());
+    tomorrow.getHours=(tomorrowD.getHours());
+    tomorrow.getMinutes=(tomorrowD.getMinutes());
+    tomorrow.getSeconds=(tomorrowD.getSeconds());
+    tomorrow.getAllPluDateNnY=(tomorrow.getMonth+tomorrow.getDate+tomorrow.getHours+tomorrow.getMinutes+tomorrow.getSeconds);
+    // データを取得
+    var req = new XMLHttpRequest();// XMLHttpRequest オブジェクトを生成する
+    req.onreadystatechange = function() {// XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
+        if(req.readyState == 4 && req.status == 200){// サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
+            serverJsonData = (JSON.parse(req.responseText));// 取得した JSON を変数に
+        }
+    };
+    req.open("GET", `${windowFileDirectlyPass}/data/json/news.json`, false);// HTTPメソッドとアクセスするサーバーの　URL　を指定
+    req.send(null);
+
+    // 処理を開始
+    let dayDataSpell = 'YEMODAHOMISE';
+    let HtmlNewsDiv = document.getElementById('news');
+
+    var news_i = 0;
+    for(let content_list_i = 0; content_list_i < (serverJsonData.content_list.length); content_list_i++){
+        let startDay = (serverJsonData.content_list[content_list_i].start);
+        let endDay = (serverJsonData.content_list[content_list_i].end);
+        let startDayObj={};
+        let endDayObj={};
+        var positionZ=0;
+        for(let startDay_i = 0; startDay_i < (dayDataSpell.length); startDay_i=startDay_i+2){
+            var key=(`${dayDataSpell[startDay_i]}${dayDataSpell[startDay_i+1]}`);
+            let position = startDay.indexOf(key);
+            startDayObj[`${key}`]=(startDay.substring(positionZ, position));
+            positionZ=position+2;
+        };
+        var positionZ=0;
+        for(let startDay_i = 0; startDay_i < (dayDataSpell.length); startDay_i=startDay_i+2){
+            var key=(`${dayDataSpell[startDay_i]}${dayDataSpell[startDay_i+1]}`)
+            let position = endDay.indexOf(key);
+            endDayObj[`${key}`]=(endDay.substring(positionZ, position));
+            positionZ=position+2;
+        };
+        var show = "";
+        var obj = convertCookieToObject(document.cookie);
+        var news_cookieData = (`news_pop_${serverJsonData.content_list[content_list_i].id}`)
+        //console.log(news_cookieData);
+        if(obj[news_cookieData] === 'true'){
+
+        }else{
+            if(startDayObj.YE === 'n' && startDayObj.MO === 'n' && startDayObj.DA === 'n' && startDayObj.HO === 'n' && startDayObj.MI === 'n' && startDayObj.SE === 'n'){
+                show='true';
+            }else{
+                if(startDayObj.YE === 'n' && (startDayObj.MO+startDayObj.DA+startDayObj.HO+startDayObj.MI+startDayObj.SE) >= tomorrow.getAllPluDateNnY){
+                    show='true';
+                }else{
+                    show='false';
+                };
+            };
+            if(show==='true'){
+                if(endDayObj.YE === 'n' && endDayObj.MO === 'n' && endDayObj.DA === 'n' && endDayObj.HO === 'n' && endDayObj.MI === 'n' && endDayObj.SE === 'n'){
+                    show='true';
+                }else{
+                    if(endDayObj.YE === 'n' && (endDayObj.MO+endDayObj.DA+endDayObj.HO+endDayObj.MI+endDayObj.SE) <= tomorrow.getAllPluDateNnY){
+                        show='false';   
+                    }else{
+                        show='true';
+                    };
+                };
+            };
+        }
+        let HtmlNewsGroupDiv = document.createElement('div');
+        let HtmlNewsContentGroupDiv = `<div class="modal-content newspaper">`
+        for(let HtmlNewsContentGroupDiv_i = 0; HtmlNewsContentGroupDiv_i < serverJsonData.content_list[content_list_i].content.length; HtmlNewsContentGroupDiv_i++){
+            let HtmlNewsContentDescriptionDiv = ``;
+            if(HtmlNewsContentGroupDiv_i === 0){
+                HtmlNewsContentDescriptionDiv=(`
+                    <div style="margin-bottom:10px;">
+                        <h1 class="h1"><strong>${serverJsonData.content_list[content_list_i].content[HtmlNewsContentGroupDiv_i].title}</strong></h1>
+                        <p>${serverJsonData.content_list[content_list_i].content[HtmlNewsContentGroupDiv_i].description}</p>
+                    </div>
+                `);
+            }else{
+                HtmlNewsContentDescriptionDiv=(`
+                    <div style="margin-bottom:10px;">
+                        <h2 style="font-size: 23px;"><strong>${serverJsonData.content_list[content_list_i].content[HtmlNewsContentGroupDiv_i].title}</strong></h2>
+                        <p>${serverJsonData.content_list[content_list_i].content[HtmlNewsContentGroupDiv_i].description}</p>
+                    </div>
+                `);
+            };
+            HtmlNewsContentGroupDiv=HtmlNewsContentGroupDiv+(HtmlNewsContentDescriptionDiv);
+        };
+        HtmlNewsContentGroupDiv=HtmlNewsContentGroupDiv+'</div>';
+        HtmlNewsGroupDiv.innerHTML=(`
+            <div data-ml-modal id="${serverJsonData.content_list[content_list_i].id}" class="fadeDown">
+                <audio src="${windowFileDirectlyPass}/assets/sounds/cursor.mp3" id="btn-sound"></audio>
+                <audio src="${windowFileDirectlyPass}/assets/sounds/checkBtn40.mp3" id="pushSound"></audio>
+                <a id="news_url_1_${serverJsonData.content_list[content_list_i].id}" href="#!" class="modal-overlay"></a>
+                <div class="modal-dialog modal-dialog-lg">
+                    <a id="news_url_2_${serverJsonData.content_list[content_list_i].id}" href="#!" class="modal-close">&times;</a>
+                    <h3><strong>${serverJsonData.content_list[content_list_i].title}</strong></h3>
+                    <div>${HtmlNewsContentGroupDiv}</div>
+                </div>  
+            </div>
+        `);
+
+        //console.log(HtmlNewsGroupDiv)
+        if(page === 'true'){
+            if(show==='true'){
+                HtmlNewsDiv.appendChild(HtmlNewsGroupDiv);
+                newsObj[`${news_i}`] = {id:serverJsonData.content_list[content_list_i].id,show:show,saveTime:serverJsonData.content_list[content_list_i].saveTime};
+                news_i = news_i+1;
+            }
+        }else{
+            if(show==='true' && serverJsonData.content_list[content_list_i].page === page){
+                HtmlNewsDiv.appendChild(HtmlNewsGroupDiv);
+                newsObj[`${news_i}`] = {id:serverJsonData.content_list[content_list_i].id,show:show,saveTime:serverJsonData.content_list[content_list_i].saveTime};
+                news_i = news_i+1;
+            }
+        }
+    };
+    newsObj.length=news_i;
+    newsload();
+};
+
+function newsload(){
+    //console.log(newsObj)
+    var newsPushObj={};
+    if(newsObj.length > 0){
+        var obj = convertCookieToObject(document.cookie);
+        for(let news_si = 0; news_si < newsObj.length; news_si++){
+            newsPushObj[`${newsObj[news_si].id}`]=(newsObj[news_si].saveTime);
+            newsPushObj[`show_${newsObj[news_si].id}`]=(newsObj[news_si].show);
+            newsObj[`${news_si}`].cookie=(`${newsObj[news_si].id}`);
+            var count = news_si + 1;
+            var btnC1 = document.getElementById(`news_url_1_${newsObj[news_si].id}`);
+            var btnC2 = document.getElementById(`news_url_2_${newsObj[news_si].id}`);
+            if(news_si < (newsObj.length-1)){
+                btnC1.setAttribute('href',`#${newsObj[count].id}`);
+                btnC2.setAttribute('href',`#${newsObj[count].id}`);
+            }else{
+                btnC2.setAttribute(`href`,`#!`);
+                btnC2.setAttribute(`href`,`#!`);
+            };
+            btnC1.addEventListener('click', () => {
+                document.getElementById('btn-sound').currentTime = 0;
+                document.getElementById('btn-sound').play();
+                if(newsObj[news_si].show === 'true'){
+                    document.cookie = `news_pop_${newsObj[news_si].id}=true; max-age=${newsObj[news_si].saveTime}; path=/`;
+                }
+            });
+            btnC2.addEventListener('click', () => {
+                document.getElementById('btn-sound').currentTime = 0;
+                document.getElementById('btn-sound').play();
+                if(newsObj[news_si].show === 'true'){
+                    document.cookie = `news_pop_${newsObj[news_si].id}=true; max-age=${newsObj[news_si].saveTime}; path=/`;
+                }
+            });
+        };
+        window.location.href=(`#${newsObj["0"].id}`);
+        var SeOneHash='';
+        var puC=newsObj.length;
+        //console.log(newsPushObj);
+        setInterval(() => {
+            if(SeOneHash!==(window.location.hash)){
+                if(puC > 0){
+                    SeOneHash = (window.location.hash);
+                    document.getElementById('pushSound').currentTime = 0;
+                    document.getElementById('pushSound').play();
+                    puC=puC-1;
+                };
+            };
+        }, 100);
+    }else{
+        window.location.href=(`#`);
+    };
+};
+
+
+function convertCookieToObject(cookies){//クッキーの解析
+    const cookieItem = cookies.split(';');
+    const obj = {};
+    cookieItem.forEach((item) => {
+        // 「=」で分解
+        const element = item.split('=');
+        // キーを取得
+        const key = element[0].trim();
+        // バリューを取得
+        const value = decodeURIComponent(element[1]);
+        // 保存
+        obj[key] = value;
+    });
+    return obj;
+};
+
+function a(){
+    let HtmlNewsContentGroupDiv = document.createElement('div');
+    HtmlNewsContentGroupDiv.classList.add('modal-content','newspaper');
+    for(let HtmlNewsContentGroupDiv_i = 0; HtmlNewsContentGroupDiv_i < serverJsonData.content_list[content_list_i].content.length; HtmlNewsContentGroupDiv_i++){
+        let HtmlNewsContentDescriptionDiv = document.createElement('div');
+        HtmlNewsContentDescriptionDiv.style.marginBottom=('10px');
+        if(HtmlNewsContentGroupDiv_i === 0){
+            HtmlNewsContentDescriptionDiv.innerHTML=(`
+                <h1 class="h1"><strong>${serverJsonData.content_list[content_list_i].content[HtmlNewsContentGroupDiv_i].title}</strong></h1>
+                <p>${serverJsonData.content_list[content_list_i].content[HtmlNewsContentGroupDiv_i].description}</p>
+            `);
+        }else{
+            HtmlNewsContentDescriptionDiv.innerHTML=(`
+                <h2 style="font-size: 23px;"><strong>${serverJsonData.content_list[content_list_i].content[HtmlNewsContentGroupDiv_i].title}</strong></h2>
+                <p>${serverJsonData.content_list[content_list_i].content[HtmlNewsContentGroupDiv_i].description}</p>
+            `);
+        };
+        HtmlNewsContentGroupDiv.appendChild(HtmlNewsContentDescriptionDiv);
+    };
+};
