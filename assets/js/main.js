96c648390da3dd53dcc7b4fdc47a4a3e4e6bfd45
Fun117 <141471450+Fun117@users.noreply.github.com>
2023-12-27 01:27:19 +0900

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme() {
-    const htmlTag = document.querySelector('html');
-    if (htmlTag.classList.contains('dark')) {
-        htmlTag.classList.remove('dark');
-    } else {
-        htmlTag.classList.add('dark');
-    }
-    class_change()
-}
